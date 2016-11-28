a003c539cf2587ebd84bd648f03a630d132741d2
Sam Wilson <sam@samwilson.id.au>
2016-11-28 10:00:56 +0800

    Version bump for Subversion fix

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ master_doc = 'index'
 project = u'Tabulate for WordPress'
 copyright = u'2016, Sam Wilson'
 version = '2.7'
-release = '2.7.14'
+release = '2.7.15'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
