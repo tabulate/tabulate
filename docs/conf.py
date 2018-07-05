e60f6fd2cdb2c4f894ed3b9646e6c3be7059c47b
Sam Wilson <sam@samwilson.id.au>
2018-07-05 19:52:11 +0800

    Version 2.10.3

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ master_doc = 'index'
 project = u'Tabulate for WordPress'
 copyright = u'2016, Sam Wilson'
 version = '2.10'
-release = '2.10.2'
+release = '2.10.3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
